f1710545161a19e9fe6e300c1e5e6ba881b43a90
Ajay Raagaventhira D <ajayraagaventhira19@gmail.com>
2026-05-26 21:51:41 +0530

    added all files

---
+++ src/main/java/com/app/devopsdemo/App.java
@@ -0,0 +1,11 @@
+package com.app.devopsdemo;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/com/app/AppTest.java
@@ -0,0 +1,14 @@
+package com.app;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    void testAddition() {
+        int sum = 2 + 3;
+        assertEquals(5, sum);
+    }
+}
